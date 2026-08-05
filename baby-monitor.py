8f35c2693a8f627964c376b030bc1fff3db3ff23
Lucas <1019firefly@gmail.com>
2026-08-05 17:22:51 +0800

    feat: 宝宝监护主循环

---
+++ baby-monitor.py
@@ -0,0 +1,8 @@
+import sounddevice as sd
+SAMPLE_RATE = 16000
+SECONDS = 1
+print("Monitoring...")
+while True:
+    audio_date = sd.rec(SAMPLE_RATE * SECONDS,samplerate = SAMPLE_RATE,channels = 1)
+    sd.wait()
+    print(abs(audio_date).max())
