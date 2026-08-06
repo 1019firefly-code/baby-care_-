d99b7ded9b46e4ed24b61f284deeab2854fe397b
Lucas <1019firefly@gmail.com>
2026-08-06 17:29:17 +0800

    feat: yamnet 模型识别声音类别

--- baby-monitor.py
+++ baby-monitor.py
@@ -1,8 +1,12 @@
 import sounddevice as sd
+THRESHOLD = 0.15
 SAMPLE_RATE = 16000
 SECONDS = 1
 print("Monitoring...")
 while True:
-    audio_date = sd.rec(SAMPLE_RATE * SECONDS,samplerate = SAMPLE_RATE,channels = 1)
+    audio_data = sd.rec(SAMPLE_RATE * SECONDS,samplerate = SAMPLE_RATE,channels = 1)
     sd.wait()
-    print(abs(audio_date).max())
+    volume = abs(audio_data).max()
+    print(volume)
+    if volume > THRESHOLD:
+        print("检测到声音")
--- mic_test.py
+++ mic_test.py
@@ -2,6 +2,6 @@ import sounddevice as sd
 SAMPLE_RATE = 16000
 SECONDS = 1
 print("Recording...")
-audio_date = sd.rec(SAMPLE_RATE * SECONDS,samplerate = SAMPLE_RATE,channels = 1)
+audio_data = sd.rec(SAMPLE_RATE * SECONDS,samplerate = SAMPLE_RATE,channels = 1)
 sd.wait()
-print(abs(audio_date).max())
+print(abs(audio_data).max())
